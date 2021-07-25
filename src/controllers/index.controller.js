b58523a35c7c15e2790a76bc9645b4d9bb3f8d4e
JairoArevalo <jairoarevalo4@gmail.com>
2021-07-25 16:40:58 -0500

    "nombres db"

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,40 +3,41 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    password: '',
-    database: 'firstapi',
+    password: '0012',
+    database: 'test',
     port: '5432'
 });
 
 const getUsers = async (req, res) => {
-    const response = await pool.query('SELECT * FROM users ORDER BY id ASC');
+    const response = await pool.query('SELECT * FROM adm.paciente ORDER BY id ASC');
     res.status(200).json(response.rows);
 };
 
 const getUserById = async (req, res) => {
     const id = parseInt(req.params.id);
-    const response = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    const response = await pool.query('SELECT * FROM adm.usuario WHERE id = $1', [id]);
     res.json(response.rows);
 };
 
 const createUser = async (req, res) => {
-    const { name, email } = req.body;
-    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+    const { name, correo } = req.body;
+    console.log("Respuesta ", req.body);
+    const response = await pool.query('INSERT INTO adm.usuario (nombre, correo) VALUES ($1, $2)', [name, correo]);
     res.json({
         message: 'User Added successfully',
         body: {
-            user: {name, email}
+            user: {name, correo}
         }
     })
 };
 
 const updateUser = async (req, res) => {
     const id = parseInt(req.params.id);
-    const { name, email } = req.body;
+    const { name, correo } = req.body;
 
-    const response =await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
+    const response =await pool.query('UPDATE adm.usuario SET name = $1, correo = $2 WHERE id = $3', [
         name,
-        email,
+        correo,
         id
     ]);
     res.json('User Updated Successfully');
@@ -44,7 +45,7 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
     const id = parseInt(req.params.id);
-    await pool.query('DELETE FROM users where id = $1', [
+    await pool.query('DELETE FROM adm.usuario where id = $1', [
         id
     ]);
     res.json(`User ${id} deleted Successfully`);
